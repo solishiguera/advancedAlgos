aef713a48e563933e9caf86626ac8cab13cb8c47
Diego <A00827847@itesm.mx>
2021-10-22 10:03:41 -0500

    hi new files

---
+++ MST/MST/act11.cpp
@@ -0,0 +1,8 @@
+//
+//  act11.cpp
+//  MST
+//
+//  Created by Diego Solis on 10/22/21.
+//
+
+#include <stdio.h>
---
+++ MST/MST/act13.cpp
@@ -0,0 +1,8 @@
+//
+//  act13.cpp
+//  MST
+//
+//  Created by Diego Solis on 10/22/21.
+//
+
+#include <stdio.h>
