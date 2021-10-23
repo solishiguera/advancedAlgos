94310d156fc1d725bb3411ef560699144f859a4a
Diego <A00827847@itesm.mx>
2021-10-22 22:36:58 -0500

    it works

--- MST/MST/act11.cpp
+++ MST/MST/act11.cpp
@@ -73,10 +73,14 @@ void calculaCostoPosible(Nodo &nodoActual, int matAdj[MAX][MAX], int n){
 int tsp(int matAdj[MAX][MAX], int n){
     int costoOptimo = INT_MAX;
     Nodo raiz;
-    raiz.niv = 0;
+    raiz.niv = 1;
     raiz.costoAcum = 0;
     raiz.verticeActual = 1;
     raiz.verticeAnterior = 0;
+    for (int i = 0; i <= n; i++) {
+        raiz.visitados[i] = false;
+    }
+    
     raiz.visitados[1] = true; // garantizar que todos los demas tengan false
     calculaCostoPosible(raiz, matAdj, n);
     priority_queue<Nodo> pq;
@@ -87,17 +91,17 @@ int tsp(int matAdj[MAX][MAX], int n){
         int vAct = raiz.verticeActual;
         if(raiz.costoPos < costoOptimo) {
             for (int i = 0; i <= n; i++) {
-                if (!raiz.visitados[i] && matAdj[i][vAct] != INT_MAX) { // Revisar esto !!!
+                if (!raiz.visitados[i] && matAdj[vAct][i] != INT_MAX) { // Revisar esto !!!
                     Nodo tmp = raiz;
-                    tmp.niv += 1;
-                    tmp.costoAcum += matAdj[i][vAct];
-                    tmp.verticeActual = 1;
-                    tmp.verticeAnterior = 0;
-                    tmp.visitados[1] = true; // garantizar que todos los demas tengan false
+                    tmp.niv++;
+                    tmp.costoAcum += matAdj[vAct][i];
+                    tmp.verticeActual = i;
+                    tmp.verticeAnterior = vAct;
+                    tmp.visitados[i] = true; // garantizar que todos los demas tengan false
                     
                     if (tmp.niv == n) {
-                        if (matAdj[vAct][1] != INT_MAX) {
-                            int costoReal = tmp.costoAcum + matAdj[vAct][1];
+                        if (matAdj[i][1] != INT_MAX) {
+                            int costoReal = tmp.costoAcum + matAdj[i][1];
                             costoOptimo = min(costoReal, costoOptimo);
                         }
                     } else {
@@ -124,3 +128,47 @@ int main(){
     leerArcos(matAdj, m);
     (tsp(matAdj, n) != INT_MAX) ? cout << tsp(matAdj, n) << endl : cout << "INF" << endl;
 }
+
+
+/*
+4 5
+A B 5
+A C 10
+A D 8
+B C 2
+C D 1
+
+5 7
+A B 4
+A C 8
+A D 2
+B C 7
+B D 2
+C D 1
+D E 3
+
+7 11
+A B 2
+A C 4
+A D 6
+B C 2
+B E 6
+C D 1
+C E 3
+D E 2
+D F 3
+E G 5
+F G 4
+
+5 10
+A B 4
+A C 8
+A D 5
+A E 3
+B C 7
+B D 2
+B E 2
+C D 1
+C E 4
+D E 3
+ */
