959f28d85adca7940800da5100a0b6ed8604be28
Diego <A00827847@itesm.mx>
2021-10-22 22:16:13 -0500

    kinda works

--- MST/MST/act11.cpp
+++ MST/MST/act11.cpp
@@ -21,7 +21,7 @@ struct Nodo{
     int verticeAnterior;
     int verticeActual;
     bool visitados[MAX];
-    bool operator<(const Nodo &otro) const{ // para que la priority queue tenga la prioridad
+    bool operator<(const Nodo &otro) const{ // priority queue tenga la prioridad de menor costo posible
         return costoPos>=otro.costoPos;
     }
 };
@@ -47,21 +47,21 @@ void leerArcos(int matAdj[MAX][MAX], int m){
 void calculaCostoPosible(Nodo &nodoActual, int matAdj[MAX][MAX], int n){
     nodoActual.costoPos = nodoActual.costoAcum;
     int costoObtenido;
-    for(int i=1; i<=n; i++){
+    for (int i=1; i<=n; i++){
         costoObtenido = INT_MAX;
-        //No he visitado al nodo i
-        if(!nodoActual.visitados[i] || i == nodoActual.verticeActual){
-            //no lo he visitado
+        // No he visitado al nodo i
+        if (!nodoActual.visitados[i] || i == nodoActual.verticeActual) {
+            // no lo he visitado
             if(!nodoActual.visitados[i]){
-                for(int j=1; j<=n; j++){
-                    if( i != j && (!nodoActual.visitados[j] || j == 1)){
-                        costoObtenido = min(costoObtenido, matAdj[i-1][j-1]);
+                for (int j=1; j<=n; j++){
+                    if (i != j && (!nodoActual.visitados[j] || j == 1)){
+                        costoObtenido = min(costoObtenido, matAdj[i][j]);
                     }
                 }
-            } else {  //soy el ultimo visitado
-                for(int j=1; j<=n; j++){
-                    if(!nodoActual.visitados[j]){
-                        costoObtenido = min(costoObtenido, matAdj[i-1][j-1]);
+            } else { // soy el último visitado
+                for (int j=1; j<=n; j++){
+                    if (!nodoActual.visitados[j]) {
+                        costoObtenido = min(costoObtenido, matAdj[i][j]);
                     }
                 }
             }
@@ -77,34 +77,50 @@ int tsp(int matAdj[MAX][MAX], int n){
     raiz.costoAcum = 0;
     raiz.verticeActual = 1;
     raiz.verticeAnterior = 0;
-    raiz.visitados[1] = true; //garantizar que todos los demas tengan false
+    raiz.visitados[1] = true; // garantizar que todos los demas tengan false
     calculaCostoPosible(raiz, matAdj, n);
     priority_queue<Nodo> pq;
     pq.push(raiz);
-    while (!pq.empty()){
-        // Try here
+    while (!pq.empty()) {
+        raiz = pq.top();
+        pq.pop();
+        int vAct = raiz.verticeActual;
+        if(raiz.costoPos < costoOptimo) {
+            for (int i = 0; i <= n; i++) {
+                if (!raiz.visitados[i] && matAdj[i][vAct] != INT_MAX) { // Revisar esto !!!
+                    Nodo tmp = raiz;
+                    tmp.niv += 1;
+                    tmp.costoAcum += matAdj[i][vAct];
+                    tmp.verticeActual = 1;
+                    tmp.verticeAnterior = 0;
+                    tmp.visitados[1] = true; // garantizar que todos los demas tengan false
+                    
+                    if (tmp.niv == n) {
+                        if (matAdj[vAct][1] != INT_MAX) {
+                            int costoReal = tmp.costoAcum + matAdj[vAct][1];
+                            costoOptimo = min(costoReal, costoOptimo);
+                        }
+                    } else {
+                        calculaCostoPosible(tmp, matAdj, n);
+                        if (tmp.costoPos < costoOptimo) {
+                            pq.push(tmp);
+                        }
+                    }
+                }
+            }
+        }
     }
+    
     return costoOptimo;
 }
 
-void printMat(int matAdj[MAX][MAX], int n){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout<<matAdj[i][j]<<"\t";
-        }
-        cout<<endl;
-    }
-}
-
 int main(){
-    //n = cantidad de nodos
-    //m = cantidad de arcos
+    // n = cantidad de nodos
+    // m = cantidad de arcos
     int n, m;
     cin >> n >> m;
-    int matAdj[MAX][MAX];//asumimos que es base 1 (A=1, B=2, C=3...)
+    int matAdj[MAX][MAX]; // asumimos que es base 1 A=1, B=2, C=3...
     iniciaMat(matAdj);
     leerArcos(matAdj, m);
-    printMat(matAdj,n);
-    cout << tsp(matAdj, n) << endl;
-
+    (tsp(matAdj, n) != INT_MAX) ? cout << tsp(matAdj, n) << endl : cout << "INF" << endl;
 }
